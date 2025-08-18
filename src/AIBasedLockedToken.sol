65c9569518c3affbc36f1a36f84db8c63c0b85fa
Saeid <saeed221373@yahoo.com>
2025-08-18 11:56:02 +0330

    spacing fix

--- src/AIBasedLockedToken.sol
+++ src/AIBasedLockedToken.sol
@@ -9,7 +9,7 @@ contract AIBasedLockedToken is ERC20, Ownable {
     uint256 public totalClaims;
     uint256 public constant MAX_CLAIMS = 100;
     uint256 public constant CLAIM_AMOUNT = 10 * 10**18;
-        mapping(address => bool) public whitelist;
+    mapping(address => bool) public whitelist;
     mapping(address => bool) public hasClaimed;
 
     constructor(address initialOwner) ERC20("AIBLockedToken", "AIBL") Ownable(initialOwner) {}
