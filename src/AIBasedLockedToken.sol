f3d43bb142582515d16a2c59d4ab68b9f2ca6115
Saeid <saeed221373@yahoo.com>
2025-08-18 11:54:57 +0330

    update sample code

--- src/AIBasedLockedToken.sol
+++ src/AIBasedLockedToken.sol
@@ -1,2 +1,36 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.20;
+
+import {ERC20} from "openzeppelin-contracts/contracts/token/ERC20/ERC20.sol";
+import {Ownable} from "openzeppelin-contracts/contracts/access/Ownable.sol";
+
+contract AIBasedLockedToken is ERC20, Ownable {
+    mapping(address => bool) public whitelist;
+    mapping(address => bool) public hasClaimed;
+    uint256 public totalClaims;
+    uint256 public constant MAX_CLAIMS = 100;
+    uint256 public constant CLAIM_AMOUNT = 10 * 10**18;
+
+    constructor(address initialOwner) ERC20("AIBLockedToken", "AIBL") Ownable(initialOwner) {}
+
+    function addToWhitelist(address[] calldata addresses) external onlyOwner {
+        for (uint256 i = 0; i < addresses.length; i++) {
+            whitelist[addresses[i]] = true;
+        }
+    }
+
+    function claim() external {
+        require(whitelist[msg.sender], "Not whitelisted");
+        require(!hasClaimed[msg.sender], "Already claimed");
+        require(totalClaims < MAX_CLAIMS, "Claim limit reached");
+
+        totalClaims++;
+        hasClaimed[msg.sender] = true;
+        _mint(msg.sender, CLAIM_AMOUNT);
+    }
+
+    function _update(address from, address to, uint256 value) internal override {
+        require(from == address(0) || to == address(0), "Token is non-transferable");
+        super._update(from, to, value);
+    }
+}
