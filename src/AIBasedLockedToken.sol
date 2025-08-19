9f836edbf4d83fbd261d93b899ecfda537474c4e
Saeid <saeed221373@yahoo.com>
2025-08-19 17:02:15 +0330

    update max supply

--- src/AIBasedLockedToken.sol
+++ src/AIBasedLockedToken.sol
@@ -6,13 +6,16 @@ import {Ownable} from "openzeppelin-contracts/contracts/access/Ownable.sol";
 
 contract AIBasedLockedToken is ERC20, Ownable {
 
+    uint256 public maxSupply;
     uint256 public totalClaims;
     uint256 public constant MAX_CLAIMS = 100;
     uint256 public constant CLAIM_AMOUNT = 10 * 10**18;
     mapping(address => bool) public whitelist;
     mapping(address => bool) public hasClaimed;
 
-    constructor(address initialOwner) ERC20("AIBLockedToken", "AIBL") Ownable(initialOwner) {}
+    constructor(address initialOwner) ERC20("AIBLockedToken", "AIBL") Ownable(initialOwner) {
+        maxSupply = MAX_CLAIMS * CLAIM_AMOUNT;
+    }
 
 
     function _update(address from, address to, uint256 value) internal override {
@@ -23,7 +26,7 @@ contract AIBasedLockedToken is ERC20, Ownable {
     function claim() external {
         require(whitelist[msg.sender], "Not whitelisted");
         require(!hasClaimed[msg.sender], "Already claimed");
-        require(totalClaims < MAX_CLAIMS, "Claim limit reached");
+        require(totalSupply() + CLAIM_AMOUNT <= maxSupply, "Max supply reached");
 
         totalClaims++;
         hasClaimed[msg.sender] = true;
@@ -34,6 +37,11 @@ contract AIBasedLockedToken is ERC20, Ownable {
         for (uint256 i = 0; i < addresses.length; i++) {
             whitelist[addresses[i]] = true;
         }
+    }
+
+    function updateMaxSupply(uint256 newMaxSupply) external onlyOwner {
+        require(newMaxSupply >= totalSupply(), "New max supply is less than total supply");
+        maxSupply = newMaxSupply;
     }
 
 }
