044f60ea86cfe7c3e58f25e980e49bcc50b361ce
saeidha <saeed221373@yahoo.com>
2025-09-02 17:46:17 +0330

    change on sample2/TokenVesting.t.sol Solidity contract Component

--- sample2/TokenVesting.t.sol
+++ sample2/TokenVesting.t.sol
@@ -139,3 +139,5 @@ contract TestTokenVesting is Test {
         vm.prank(owner);
         tokenVesting.createVestingSchedule(beneficiary1, VESTING_AMOUNT_1, startTime, DURATION, CLIFF);
 
+        // First release
+        uint64 firstReleaseTime = startTime + CLIFF + 30 days;
