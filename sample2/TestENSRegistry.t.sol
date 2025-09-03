fa0947c2297cb16cef9e848e1c9c39076ba09b4a
saeidha <saeed221373@yahoo.com>
2025-09-03 19:14:33 +0330

    change on sample2/TestENSRegistry.t.sol Solidity contract Adjustment

--- sample2/TestENSRegistry.t.sol
+++ sample2/TestENSRegistry.t.sol
@@ -47,3 +47,6 @@ contract TestENSRegistry is Test {
 
     function test_setSubnodeOwner() public {
         vm.prank(user1);
+        registry.setSubnodeOwner(testNode, testLabel, user2);
+        assertEq(registry.owner(testSubNode), user2);
+    }
