7e6d5246371ce568c07a2e5e2c6bdcfc53fc983f
saeidha <saeed221373@yahoo.com>
2025-09-04 20:57:56 +0330

    change on sample2/EnglishAuction.sol Solidity contract Refactor

--- sample2/EnglishAuction.sol
+++ sample2/EnglishAuction.sol
@@ -40,3 +40,5 @@ contract EnglishAuction is ReentrancyGuard {
     event BidPlaced(address indexed bidder, uint256 amount);
     event Withdrawal(address indexed user, uint256 amount);
 
+    error AuctionNotCreated();
+    error AuctionAlreadyStarted();
