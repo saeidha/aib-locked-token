8f931bcf3bf86b8f7e4a25b36789eb8df9ae95ca
saeidha <saeed221373@yahoo.com>
2025-09-08 22:12:34 +0330

    Feature change on src/TimeLockWallet.sol Solidity contract

--- src/TimeLockWallet.sol
+++ src/TimeLockWallet.sol
@@ -27,3 +27,5 @@ contract TimeLockWallet is Ownable, Pausable {
     //                         State Variables
     // =============================================================
 
+    mapping(address => Beneficiary) public beneficiaries;
+    uint256 public beneficiaryCount; // Tracks the number of *active* beneficiaries
